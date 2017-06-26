eaf90d91ebf5a0659a129f72cd9ef5726fc2b0a0
fdodino <fernando.dodino@gmail.com>
2017-06-26 11:46:44 -0300

    Another test

--- karma.conf.js
+++ karma.conf.js
@@ -62,8 +62,16 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: [
+      'ChromeNoSandbox'
+    ],
 
+    customLaunchers: {
+      ChromeNoSandbox: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
